5eb3f0adbea0b143a4bea569a46f57eaf2bfa59f
cierrajenn <cierrajenn613@gmail.com>
2024-01-30 11:22:00 -0500

    created multiple links connected by joints

--- generate.py
+++ generate.py
@@ -1,36 +1,26 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
-
-# original box position
-x = 0
-y = 0
-z = 0.5
-
-# columns 
-for _ in range(5):
-    # reset x for start of new row
-    x = 0
-    
-    # rows
-    for _ in range(5):
-        # reset z for start of new tower
-        z = 0.5
-        # original box dimensions
-        length = 1
-        height = 1
-        width = 1
-
-        # creates each tower of 10
-        for _ in range(10):
-            pyrosim.Send_Cube(name = "Box", pos = [x, y, z], size = [length, width, height])
-            # update cube dimensions
-            length *= .9
-            height *= .9
-            width *= .9
-            z += 1
-        x += 1
-    y += 1
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name = "World", pos = [5, 5, 0.5], size = [1, 1, 1])
+    pyrosim.End()
     
 
-pyrosim.End()
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name = "FrontLeg", pos = [0, 0, 0.5], size = [1, 1, 1])
+    
+    pyrosim.Send_Joint(name = "FrontLeg_Torso", parent = "FrontLeg", child = "Torso",
+                       type = "revolute", position = [0, 0, 1])
+    
+    pyrosim.Send_Cube(name = "Torso", pos = [0, 0.5, 1], size = [1, 1, 1])
+    
+    pyrosim.Send_Joint(name = "Torso_BackLeg", parent = "Torso", child = "BackLeg",
+                       type = "revolute", position = [0, 0, 1])
+    
+    pyrosim.Send_Cube(name = "BackLeg", pos = [0, -0.5, -0.5], size = [1,1,1])
+
+    pyrosim.End()
+
+Create_World()
+Create_Robot()
--- simulation.py
+++ simulation.py
@@ -5,8 +5,9 @@ physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 p.setGravity(0, 0, -9.8)
+robotId = p.loadURDF("body.urdf")
 planeId = p.loadURDF("plane.urdf")
-p.loadSDF("box.sdf")
+p.loadSDF("world.sdf")
 
 for _ in range(1000):
     p.stepSimulation()
