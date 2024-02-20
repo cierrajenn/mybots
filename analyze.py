e2c06adb556e3e3534c1413326a01f682f0b4c42
cierrajenn <cierrajenn613@gmail.com>
2024-02-19 22:37:30 -0500

    got robot to move consistently in one direction

--- analyze.py
+++ analyze.py
@@ -4,14 +4,15 @@ import numpy as np
 backLegSensorValues = np.load('data/backLegSensorVals.npy')
 frontLegSensorValues = np.load('data/frontLegSensorVals.npy')
 motorValues = np.load('data/motorVals.npy')
+sclMotorValues = np.load('data/scaledMotorVals.npy')
+newVals = np.load('data/newMotorVals.npy')
 
-plot.plot(motorValues, np.sin(motorValues))
-plot.axis('tight')
+#plot.plot(newVals, np.sin(newVals))
+#plot.show()
+
+plot.plot(backLegSensorValues, linewidth = 2, label = 'backLegSensorValues')
+plot.plot(frontLegSensorValues, linewidth = 2, label = 'frontLegSensorValues')
+plot.legend()
 plot.show()
-
-##plot.plot(backLegSensorValues, linewidth = 2, label = 'backLegSensorValues')
-##plot.plot(frontLegSensorValues, linewidth = 2, label = 'frontLegSensorValues')
-##plot.legend()
-##plot.show()
 
 #print(backLegSensorValues)
--- simulation.py
+++ simulation.py
@@ -2,6 +2,15 @@ import pybullet as p
 import pyrosim.pyrosim as pyrosim
 import numpy as np
 import pybullet_data, time, math, random
+import matplotlib.pyplot as plt
+
+blAmplitude = np.pi/9
+blFrequency = 0.08
+blPhaseOffset = -np.pi/2
+
+flAmplitude = np.pi/15
+flFrequency = 0.08
+flPhaseOffset = 0
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -16,9 +25,19 @@ pyrosim.Prepare_To_Simulate(robotId)
 backLegSensorValues = np.zeros(1000)
 frontLegSensorValues = np.zeros(1000)
 
-targetAngles = np.linspace(-math.pi/4, math.pi/4, 1000)
+targetAngles = np.linspace(0, 2, 1000)
 np.save('data/motorVals.npy', targetAngles)
-exit()
+scaled_targetAngles = np.interp(targetAngles, (targetAngles.min(), targetAngles.max()), (-np.pi/4, np.pi/4))
+np.save('data/scaledMotorVals.npy', scaled_targetAngles)
+##print(targetAngles)
+
+blVec = np.zeros(1000)
+for i in range(1000):
+    blVec[i] = blAmplitude * np.sin(blFrequency * i + blPhaseOffset)
+
+flVec = np.zeros(1000)
+for i in range(1000):
+    flVec[i] = flAmplitude * np.sin(flFrequency * i + flPhaseOffset)
 
 for _ in range(1000):
     # save each touch sensor value for the back and front legs
@@ -28,16 +47,16 @@ for _ in range(1000):
     # create motors for front and back legs
     pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = 'Torso_BackLeg',
                                 controlMode = p.POSITION_CONTROL,
-                                targetPosition = targetAngles[_],
+                                targetPosition = blVec[_],
                                 maxForce = 50)
     
     pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = 'Torso_FrontLeg',
                                 controlMode = p.POSITION_CONTROL,
-                                targetPosition = targetAngles[_],
+                                targetPosition = flVec[_],
                                 maxForce = 50)
     
     p.stepSimulation()
-    time.sleep(.016)
+    time.sleep(.00042)
 
 # save back and front leg touch sensor values ot their respective files
 np.save('data/backLegSensorVals.npy', backLegSensorValues)
